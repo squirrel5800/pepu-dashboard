e2da769a36823d6a2c41c3cdb16ca73024275aa0
squirrel5800 <markawhiteman@gmail.com>
2025-05-11 12:58:23 +0300

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -3,7 +3,7 @@ import requests
 
 # === CONFIG ===
 DEXSCREENER_URL = "https://api.dexscreener.com/latest/dex/pairs/ethereum/0x3ebec0a1b4055c8d1180fce64db2a8c068170880"
-TOKEN_HOLDINGS = 25768361
+TOKEN_HOLDINGS = 25768361.2
 
 st.set_page_config(page_title="PEPU Price Dashboard", layout="centered")
 st.title("📊 Marks Live Pepu Value")
